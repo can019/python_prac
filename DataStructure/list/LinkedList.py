1a8cd0afa32a262a61f07fed2265c66dd4bcb8af
can019 <jys01012@gmail.com>
2020-04-13 23:49:23 +0900

    add isEmpty(), size()

--- DataStructure/list/LinkedList.py
+++ DataStructure/list/LinkedList.py
@@ -2,6 +2,17 @@ from . import Node
 class LinkedList :
     dummy_head = None
     dummy_tail = None
+    size = 0
     def __init__(self): #Constructor
         self.dummy_head = Node()
         self.dummy_tail = Node()
+        self.size = int
+    def add(self, data):
+        return 0
+    def isEmpty(self):
+        if self.size() is 0:
+            return True
+        else:
+            return False
+    def size(self):
+        return self.size
