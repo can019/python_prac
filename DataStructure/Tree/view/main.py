a5c6224b36dc277639229c81ec7ae676733d23bf
can019 <jys01012@gmail.com>
2020-04-16 20:23:21 +0900

    add Remark

--- DataStructure/Tree/view/main.py
+++ DataStructure/Tree/view/main.py
@@ -1,2 +1,12 @@
+#
+# V.0.0.0.1
+# Made By Usung_Jung
+# This code is none copyright code
+# Do not erase this Remark
+# -------------------------------------
+# jys01012@gmail.com
+#
+#
+
 if __name__ == "__main__":
     print("hi");
