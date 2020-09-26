07f391a91c3ca4e2e3aba6d29127551644e24b53
can019 <jys01012@gmail.com>
2020-09-26 18:02:42 +0900

    sharpening_filtering_1

---
+++ ComputerGraphics/week3/sharpening_filter_1/__main__.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+def my_average_filter_3x3(src):
+    mask = np.array([
+        [-1/9, -1/9, 1 / -1/9],
+        [-1/9, 17/9, 1 / -1/9],
+        [-1/9, -1/9, 1 / -1/9]])
+    """
+    0  0  0       1/9  1/9  1/9     -1/9  -1/9  -1/9 
+    0  2  0   -   1/9  1/9  1/9  =  -1/9  17/9  -1/9
+    0  0  0       1/9  1/9  1/9     -1/9  -1/9  -1/9
+    """
+
+    dst = cv2.filter2D(src, -1, mask)
+    return dst
+
+if __name__ == '__man__':
+    src = cv2.imread('Lena.png', cv2.IMREAD_GRAYSCALE)
+    dst = my_average_filter_3x3(src)
+
+    cv2.imshow('original', src)
+    cv2.imshow('average filter', dst)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
